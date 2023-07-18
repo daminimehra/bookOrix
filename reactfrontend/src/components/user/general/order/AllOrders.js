0fb25ffc7082be6551aa0d13934c43abc966ace2
Damini Mehra <71871235+daminimehra@users.noreply.github.com>
2023-07-18 10:55:13 +0530

    Update AllOrders.js

--- reactfrontend/src/components/user/general/order/AllOrders.js
+++ reactfrontend/src/components/user/general/order/AllOrders.js
@@ -1,35 +1,24 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Component } from "react";
-import { Navigate } from "react-router-dom";
-import { baseUser, baseImageUrl } from "../../../../config/config";
+import { baseUser } from "../../../../config/config";
 import BeatLoader from "react-spinners/BeatLoader";
 import qs from "qs";
-import { Link } from "react-router-dom";
-//import  baseUser from '../../../../constants'
-class Allorders extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      order: [],
-      img: "book_image",
-      isLoggedIn: true,
-      readyToRedirect: false,
-      loading: true,
-      isBook: false,
-      uidId: null,
-      css: {
-        display: "block",
-        margin: "auto",
-        "border-color": "blue",
-        "margin-left": "550px",
-      },
-    };
-  }
 
-  componentDidMount() {
-    this.setState({ loading: true });
+const Allorders = () => {
+  const [order, setOrder] = useState([]);
+  const [isLoggedIn] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [css] = useState({
+    display: "block",
+    margin: "auto",
+    borderColor: "blue",
+    marginLeft: "550px",
+  });
+
+  useEffect(() => {
+    setLoading(true);
     const token = sessionStorage.getItem("token");
     const uidId = sessionStorage.getItem("uid");
     let url = `${baseUser}/order/all`;
@@ -39,50 +28,42 @@ class Allorders extends Component {
     axios
       .post(url, qs.stringify(postData), {
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+           "Content-Type": "application/x-www-form-urlencoded",
           authorization: token,
         },
       })
       .then(({ data }) => {
-        this.setState({ loading: false });
-
+        setLoading(false);
         if (data.success) {
-          // toast.success(data.message)
-          this.setState({ order: data.data });
+          setOrder(data.data);
         } else {
           toast.warning(data.message);
         }
       })
       .catch((err) => {
-        this.setState({ loading: false });
-
+        setLoading(false);
         console.log("Error in ", err);
       });
-  }
-  state = { redirect: null };
+  }, []);
 
-  render() {
-    if (this.state.loading) {
-      return (
-        <>
-          <BeatLoader
-            loading={this.state.loading}
-            css={this.state.css}
-            size={15}
-          />
-        </>
-      );
-    } else {
-      return (
-        <>
-          <ToastContainer />
-          <div className="row">
-            <div class="col-md-8 offset-2">
-              <table
-                className="table table-bordered"
-                style={{ borderRadius: "5px" }}
-              >
-                <tr style={{ color: "white" }}>
+  if (loading) {
+    return (
+      <>
+        <BeatLoader loading={loading} css={css} size={15} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <ToastContainer />
+        <div className="row">
+          <div className="col-md-8 offset-2">
+            <table
+              className="table table-bordered"
+              style={{ borderRadius: "5px" }}
+            >
+              <thead style={{ color: "white" }}>
+                <tr>
                   <th>
                     <h4>Sno</h4>{" "}
                   </th>
@@ -102,14 +83,16 @@ class Allorders extends Component {
                     <h4>Book Name </h4>
                   </th>
                 </tr>
-                {this.state.order.map((element, index) => (
+              </thead>
+              <tbody>
+                {order.map((element, index) => (
                   <tr key={index + 1}>
                     <td>{index + 1}</td>
                     <td>{element.address}</td>
                     <td>
-                      {element.orderStatus == 1
+                      {element.orderStatus === 1
                         ? "Order Placed"
-                        : element.orderStatus == 2
+                        : element.orderStatus === 2
                         ? "Order Confirmed"
                         : "Order Delivered"}
                     </td>
@@ -117,12 +100,13 @@ class Allorders extends Component {
                     <td>{element.bookId.name}</td>
                   </tr>
                 ))}
-              </table>
-            </div>
+              </tbody>
+            </table>
           </div>
-        </>
-      );
-    }
+        </div>
+      </>
+    );
   }
-}
+};
+
 export default Allorders;
